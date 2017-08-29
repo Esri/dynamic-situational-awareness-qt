51800127d38cd085a1721a1d0691f9a527810ae7
James Ballard <jballard@esri.com>
2017-08-29 14:05:57 -0700

    Typo.

--- Vehicle/main.cpp
+++ Vehicle/main.cpp
@@ -92,7 +92,7 @@ int main(int argc, char *argv[])
   //    return 1;
   //  }
 
-  // register the solutions types
+  // register the toolkit types
   ArcGISRuntimeToolkit::registerToolkitTypes();
 
   // Register the map view for QML

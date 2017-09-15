85e17678f547a0586e52a3f3ccb71f5d07f8e688
AVWORLD\mich5148 <mtims@esri.com>
2017-09-14 21:58:08 -0700

    Add copyrights

--- MessageSimulator/MessageSimulatorController.cpp
+++ MessageSimulator/MessageSimulatorController.cpp
@@ -1,10 +1,21 @@
+// Copyright 2017 ESRI
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// You may freely redistribute and use this sample code, with or
+// without modification, provided you include the original copyright
+// notice and use restrictions.
+//
+// See the Sample code usage restrictions document for further information.
+//
+
 #include "MessageSimulatorController.h"
 #include "MessageSender.h"
 #include "CoTMessageParser.h"
 #include "SimulatedMessage.h"
 #include "SimulatedMessageListModel.h"
 
-#include <QtMath>
 #include <QSettings>
 
 MessageSimulatorController::MessageSimulatorController(QObject* parent) :

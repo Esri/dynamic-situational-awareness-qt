fa1f32be79f61f1779ad887d308902fb6d6f5e2d
AVWORLD\luke8660 <lsmallwood@esri.com>
2018-02-02 10:33:00 +0000

    fix another possible assert

--- Shared/PointHighlighter.cpp
+++ Shared/PointHighlighter.cpp
@@ -64,6 +64,12 @@ void PointHighlighter::startHighlight()
       return;
     }
 
+    if (!m_highlightOverlay || !m_highlightOverlay->graphics() || m_highlightOverlay->graphics()->isEmpty())
+    {
+      m_highlightTimer->stop();
+      return;
+    }
+
     Graphic* graphic = m_highlightOverlay->graphics()->first();
     if (!graphic)
     {

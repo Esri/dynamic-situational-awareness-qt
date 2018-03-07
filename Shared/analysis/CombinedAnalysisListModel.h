0e0b618793e9ed50099095bfa25f5e30c7439bb9
AVWORLD\luke8660 <lsmallwood@esri.com>
2018-03-07 16:09:16 +0000

    formatting

--- Shared/analysis/CombinedAnalysisListModel.h
+++ Shared/analysis/CombinedAnalysisListModel.h
@@ -51,9 +51,9 @@ public:
   void removeAt(int index);
   Esri::ArcGISRuntime::Point locationAt(int index);
 
-  int rowCount(const QModelIndex &parent) const override;
-  QVariant data(const QModelIndex &index, int role) const override;
-  bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+  int rowCount(const QModelIndex& parent) const override;
+  QVariant data(const QModelIndex& index, int role) const override;
+  bool setData(const QModelIndex& index, const QVariant& value, int role) override;
 
 protected:
   QHash<int, QByteArray> roleNames() const override;

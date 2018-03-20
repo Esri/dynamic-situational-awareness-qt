95c43d98e593116c5900a0211a342b1269085dc0
AVWORLD\luke8660 <lsmallwood@esri.com>
2018-03-20 09:47:04 +0000

    update files in pch.hpp

--- Shared/pch.hpp
+++ Shared/pch.hpp
@@ -1,7 +1,25 @@
-#ifndef DSA_PCH_HPP
-#define DSA_PCH_HPP
 
-// Qt
+
+// example app headers
+#include "AlertCondition.h"
+#include "AlertFilter.h"
+#include "AlertLevel.h"
+#include "AlertTarget.h"
+
+// toolkit headers
+#include "AbstractTool.h"
+#include "ToolManager.h"
+#include "ToolResourceProvider.h"
+
+// C++ API headers
+#include "GeoElement.h"
+#include "GeometryTypes.h"
+#include "Point.h"
+#include "Scene.h"
+#include "SceneView.h"
+#include "TaskWatcher.h"
+
+// Qt headers
 #include <QAbstractListModel>
 #include <QHash>
 #include <QList>
@@ -9,22 +27,15 @@
 #include <QPointer>
 #include <QVariantMap>
 
+#ifndef DSA_PCH_HPP
+#define DSA_PCH_HPP
+
+// Qt
+
 // Runtime SDK
-#include "GeoElement.h"
-#include "GeometryTypes.h"
-#include "Point.h"
-#include "SceneQuickView.h"
-#include "Scene.h"
-#include "TaskWatcher.h"
 
 // DSA
-#include "AlertCondition.h"
-#include "AlertFilter.h"
-#include "AlertLevel.h"
-#include "AlertTarget.h"
 
 // Toolkit
-#include "AbstractTool.h"
-#include "ToolManager.h"
 
 #endif // DSA_PCH_HPP

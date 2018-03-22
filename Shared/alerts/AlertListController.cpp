b80ceb6e3c04250f03912a1e71b9e25fd48d71dd
AVWORLD\luke8660 <lsmallwood@esri.com>
2018-03-22 10:14:10 +0000

    pch

--- Shared/alerts/AlertListController.cpp
+++ Shared/alerts/AlertListController.cpp
@@ -10,10 +10,10 @@
 // See the Sample code usage restrictions document for further information.
 //
 
-#include "AlertListController.h"
-
 // PCH header
 #include "pch.hpp"
+
+#include "AlertListController.h"
 
 // example app headers
 #include "AlertConditionData.h"

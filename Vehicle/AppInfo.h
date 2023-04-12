b0c183db543673ea210e724c1f521f9267a09156
Lucas Danzinger <ldanzinger@esri.com>
2023-04-12 14:38:40 -0500

    updating version

--- Vehicle/AppInfo.h
+++ Vehicle/AppInfo.h
@@ -22,7 +22,7 @@
 #define kOrganizationDomain             "esri.com"
 
 #define kApplicationName                "DSA_Vehicle_Qt"
-#define kApplicationVersion             "1.1.6"
+#define kApplicationVersion             "1.1.7"
 #define kApplicationDescription         "Dynamic Situational Awareness - Vehicle app"
 
 #define kApplicationSourceUrl           "qrc:/qml/main.qml"

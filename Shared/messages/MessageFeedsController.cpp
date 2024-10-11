f2f6c2e39b03bc97b58c19af350c807555a99ca6
Douglas Tedder <dtedder@esri.com>
2024-10-11 15:49:05 -0400

    moving the call to add the newly created messageFeed above the addition of the DynamicEntityLayer. this will increase the count of the list above zero which will prevent any further updates from setProperties from triggering another entry into setupFeeds

--- Shared/messages/MessageFeedsController.cpp
+++ Shared/messages/MessageFeedsController.cpp
@@ -191,6 +191,7 @@ void MessageFeedsController::setupFeeds()
     const auto surfacePlacement = messageFeedJsonObject[MessageFeedConstants::MESSAGE_FEEDS_PLACEMENT].toString();
 
     auto* feed = new MessageFeed(feedName, feedType, this);
+    m_messageFeeds->append(feed);
     auto* overlay = new MessagesOverlay(feed, feedType, this);
     overlay->setSceneProperties(LayerSceneProperties(toSurfacePlacement(surfacePlacement)));
     overlay->setRenderer(createRenderer(rendererInfo, this));
@@ -206,8 +207,6 @@ void MessageFeedsController::setupFeeds()
       else
         emit toolErrorOccurred(QString("Failed to find icon %1").arg(rendererThumbnail), QString("Could not find icon %1 for feed %2").arg(rendererThumbnail, feedName));
     }
-
-    m_messageFeeds->append(feed);
   }
 
   // only needs to be cached until the geoView is ready
